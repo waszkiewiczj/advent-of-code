6d6289d32b32434c5afff25e1c80dabf8cec243f
Jakub Waszkiewicz <->
2024-12-08 02:04:37 +0100

    2024/01 part1

---
+++ 2024/01/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Push(x any)        { *h = append(*h, x.(int)) }
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func parseInput(filepath string) ([][]int, error) {
+	result := make([][]int, 2)
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		for idx, numberRaw := range strings.Fields(line) {
+			number, err := strconv.Atoi(numberRaw)
+			if err != nil {
+				return nil, err
+			}
+			result[idx] = append(result[idx], number)
+		}
+		fmt.Println()
+	}
+
+	if err = scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func part1(input [][]int) int {
+	left := (*IntHeap)(&input[0])
+	heap.Init(left)
+	right := (*IntHeap)(&input[1])
+	heap.Init(right)
+
+	result := 0
+	for left.Len() > 0 && right.Len() > 0 {
+		l := heap.Pop(left).(int)
+		r := heap.Pop(right).(int)
+		diff := int(math.Abs(float64(l) - float64(r)))
+		result += diff
+	}
+	return result
+}
+
+func main() {
+	input, err := parseInput("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	p1 := part1(input)
+	fmt.Printf("PART 1: %v", p1)
+}
