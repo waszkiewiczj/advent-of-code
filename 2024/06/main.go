5a47fa003426259a9e7d1c7797e1eb21fa9107a7
Jakub Waszkiewicz <github.fb8ck@simplelogin.com>
2024-12-12 14:20:04 +0100

    202406 part2

--- 2024/06/main.go
+++ 2024/06/main.go
@@ -31,6 +31,13 @@ type Coordinates struct {
 	X, Y int
 }
 
+func (c *Coordinates) clone() Coordinates {
+	return Coordinates{
+		X: c.X,
+		Y: c.Y,
+	}
+}
+
 type Map struct {
 	data          []string
 	width, height int
@@ -54,8 +61,14 @@ func (m *Map) visit(c Coordinates) {
 	m.visited[c.X][c.Y] = true
 }
 
-func (m *Map) get(c Coordinates) string {
-	return string(m.data[c.X][c.Y])
+func (m *Map) get(c Coordinates) byte {
+	return m.data[c.X][c.Y]
+}
+
+func (m *Map) set(c Coordinates, b byte) {
+	line := []rune(m.data[c.X])
+	line[c.Y] = rune(b)
+	m.data[c.X] = string(line)
 }
 
 func (m *Map) inBounds(c Coordinates) bool {
@@ -74,8 +87,15 @@ func (m *Map) countVisited() int {
 	return result
 }
 
-type Guard struct {
+var startDirection Coordinates = Coordinates{-1, 0}
+
+type PositionLog struct {
 	position, direction Coordinates
+}
+
+type Guard struct {
+	startPosition, position, direction Coordinates
+	log                                []PositionLog
 }
 
 func (g *Guard) rotate() {
@@ -83,18 +103,37 @@ func (g *Guard) rotate() {
 }
 
 func (g *Guard) move(m Map) {
+	g.log = append(g.log, PositionLog{
+		position:  g.position.clone(),
+		direction: g.direction.clone(),
+	})
 	newPosition := Coordinates{
 		g.position.X + g.direction.X,
 		g.position.Y + g.direction.Y,
 	}
 	if m.inBounds(newPosition) {
-		if m.get(newPosition) == "#" {
+		if m.get(newPosition) == '#' {
 			g.rotate()
 			return
 		}
 		m.visit(newPosition)
 	}
 	g.position = newPosition
+}
+
+func (g *Guard) reset() {
+	g.position = g.startPosition.clone()
+	g.direction = startDirection.clone()
+	g.log = []PositionLog{}
+}
+
+func (g *Guard) isLoop() bool {
+	for _, l := range g.log {
+		if l.direction == g.direction && l.position == g.position {
+			return true
+		}
+	}
+	return false
 }
 
 func findStartingPosition(m []string) Coordinates {
@@ -108,11 +147,21 @@ func findStartingPosition(m []string) Coordinates {
 	return Coordinates{-1, -1}
 }
 
-func part1(input []string) int {
+func initData(input []string) (Guard, Map) {
 	pos := findStartingPosition(input)
-	g := Guard{position: pos, direction: Coordinates{-1, 0}}
+	g := Guard{
+		startPosition: pos,
+		position:      pos,
+		direction:     startDirection,
+		log:           []PositionLog{},
+	}
 	m := NewMap(input)
 	m.visit(pos)
+	return g, m
+}
+
+func part1(input []string) int {
+	g, m := initData(input)
 	for m.inBounds(g.position) {
 		g.move(m)
 	}
@@ -120,8 +169,40 @@ func part1(input []string) int {
 	return result
 }
 
+func isLoop(g Guard, m Map) bool {
+	g.reset()
+	for m.inBounds(g.position) {
+		g.move(m)
+		if g.isLoop() {
+			return true
+		}
+	}
+	return false
+}
+
+func part2(input []string) int {
+	result := 0
+	g, m := initData(input)
+	for i := 0; i < m.height; i++ {
+		for j := 0; j < m.width; j++ {
+			c := Coordinates{i, j}
+			if g.startPosition != c {
+				oldChar := m.get(c)
+				m.set(c, '#')
+				if isLoop(g, m) {
+					result++
+				}
+				m.set(c, oldChar)
+			}
+
+		}
+	}
+	return result
+}
+
 func main() {
 	input := parseInput("input.txt")
 
 	fmt.Printf("PART 1: %v\n", part1(input))
+	fmt.Printf("PART 2: %v\n", part2(input))
 }
