a0ee5417823f99635319e1c47819f5dcab456e1f
Jakub Waszkiewicz <->
2024-12-14 00:10:35 +0100

    202408 part2

--- 2024/08/main.go
+++ 2024/08/main.go
@@ -87,8 +87,51 @@ func part1(m Map) int {
 	return result
 }
 
+func part2(m Map) int {
+	antinodes := [][]int{}
+	for i := 0; i < m.height; i++ {
+		antinodes = append(antinodes, make([]int, m.width))
+	}
+	result := 0
+	for _, coords := range m.GetAntennas() {
+		result++
+		for idx, c1 := range coords[:len(coords)-1] {
+			result++
+			for _, c2 := range coords[idx+1:] {
+				if c1.X > c2.X {
+					c1, c2 = c2, c1
+				}
+				distX := c2.X - c1.X
+				distY := c2.Y - c1.Y
+
+				antenna1 := Coordinates{c1.X - distX, c1.Y - distY}
+				for antenna1.X >= 0 && antenna1.X < m.height && antenna1.Y >= 0 && antenna1.Y < m.width {
+					if antinodes[antenna1.X][antenna1.Y] == 0 && m.data[antenna1.X][antenna1.Y] == '.' {
+						result++
+					}
+					antinodes[antenna1.X][antenna1.Y]++
+					antenna1.X -= distX
+					antenna1.Y -= distY
+				}
+
+				antenna2 := Coordinates{c2.X + distX, c2.Y + distY}
+				for antenna2.X >= 0 && antenna2.X < m.height && antenna2.Y >= 0 && antenna2.Y < m.width {
+					if antinodes[antenna2.X][antenna2.Y] == 0 && m.data[antenna2.X][antenna2.Y] == '.' {
+						result++
+					}
+					antinodes[antenna2.X][antenna2.Y]++
+					antenna2.X += distX
+					antenna2.Y += distY
+				}
+			}
+		}
+	}
+	return result
+}
+
 func main() {
 	input := parseInput("input.txt")
 
 	fmt.Printf("PART 1: %v\n", part1(input))
+	fmt.Printf("PART 2: %v\n", part2(input))
 }
