99ed5cb1a911164dd7856498e490a91106e81530
Jakub Waszkiewicz <github.fb8ck@simplelogin.com>
2024-12-19 09:33:03 +0100

    refactor 202410 part1

--- 2024/10/main.go
+++ 2024/10/main.go
@@ -4,8 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-
-	"github.com/dominikbraun/graph"
 )
 
 func parseInput(filepath string) [][]int {
@@ -26,72 +24,53 @@ func parseInput(filepath string) [][]int {
 	return result
 }
 
-func iterateOverElements(data [][]int, f func(x, y, val int)) {
+func getStartPoints(data [][]int) [][2]int {
+	starts := [][2]int{}
 	for x, row := range data {
 		for y, val := range row {
-			f(x, y, val)
+			if val == 0 {
+				starts = append(starts, [2]int{x, y})
+			}
+		}
+	}
+	return starts
+}
+
+func dfs(data [][]int, x, y int, stop func(val, x, y int) bool) {
+	if !stop(data[x][y], x, y) {
+		for _, d := range [][]int{{1, 0}, {0, 1}, {-1, 0}, {0, -1}} {
+			xx := x + d[0]
+			yy := y + d[1]
+			if xx >= 0 && xx < len(data) &&
+				yy >= 0 && yy < len(data[0]) &&
+				data[xx][yy]-data[x][y] == 1 {
+				dfs(data, xx, yy, stop)
+			}
 		}
 	}
 }
 
-func getVertexLabel(x, y int) string {
-	return fmt.Sprintf("%v-%v", x, y)
-}
-
-func initGraph(data [][]int) graph.Graph[string, string] {
-	g := graph.New(graph.StringHash, graph.Directed())
-
-	iterateOverElements(data, func(x, y, val int) {
-		label := getVertexLabel(x, y)
-		g.AddVertex(label)
-	})
-
-	iterateOverElements(data, func(x, y, val int) {
-		label := getVertexLabel(x, y)
-		for _, d := range [][]int{
-			{1, 0},
-			{0, 1},
-			{-1, 0},
-			{0, -1},
-		} {
-			xx := x + d[0]
-			yy := y + d[1]
-			if xx >= 0 && yy >= 0 && xx < len(data) && yy < len(data[0]) && data[xx][yy]-val == 1 {
-				g.AddEdge(label, fmt.Sprintf("%v-%v", xx, yy))
-			}
-		}
-	})
-	return g
-}
-
 func part1(data [][]int) int {
-	g := initGraph(data)
-	starts := [][]int{}
-	ends := [][]int{}
-	iterateOverElements(data, func(x, y, val int) {
-		if val == 0 {
-			starts = append(starts, []int{x, y})
-		}
-		if val == 9 {
-			ends = append(ends, []int{x, y})
-		}
-	})
-
+	starts := getStartPoints(data)
 	result := 0
 	for _, start := range starts {
-		startLabel := fmt.Sprintf("%v-%v", start[0], start[1])
-		for _, end := range ends {
-			endLabel := fmt.Sprintf("%v-%v", end[0], end[1])
-			_, err := graph.ShortestPath(g, startLabel, endLabel)
-			if err == nil {
+		visited := make(map[[2]int]bool)
+		stop := func(val, x, y int) bool {
+			shouldStop := val == 9
+			pos := [2]int{x, y}
+			_, ok := visited[pos]
+			if shouldStop && !ok {
 				result++
+				visited[pos] = true
 			}
+			return shouldStop
 		}
+		dfs(data, start[0], start[1], stop)
 	}
 	return result
 }
 
-func dfs(data [][]int, x, y int, result *int) {
+func dfs2(data [][]int, x, y int, result *int) {
 	if data[x][y] == 9 {
 		*result++
 	} else {
@@ -101,22 +80,17 @@ func dfs(data [][]int, x, y int, result *int) {
 			if xx >= 0 && xx < len(data) &&
 				yy >= 0 && yy < len(data[0]) &&
 				data[xx][yy]-data[x][y] == 1 {
-				dfs(data, xx, yy, result)
+				dfs2(data, xx, yy, result)
 			}
 		}
 	}
 }
 
 func part2(data [][]int) int {
-	starts := [][]int{}
-	iterateOverElements(data, func(x, y, val int) {
-		if val == 0 {
-			starts = append(starts, []int{x, y})
-		}
-	})
+	starts := getStartPoints(data)
 	result := 0
 	for _, start := range starts {
-		dfs(data, start[0], start[1], &result)
+		dfs2(data, start[0], start[1], &result)
 	}
 	return result
 }
@@ -124,6 +98,6 @@ func part2(data [][]int) int {
 func main() {
 	input := parseInput("input.txt")
 
-	// fmt.Printf("PART 1: %v\n", part1(input))
+	fmt.Printf("PART 1: %v\n", part1(input))
 	fmt.Printf("PART 2: %v\n", part2(input))
 }
